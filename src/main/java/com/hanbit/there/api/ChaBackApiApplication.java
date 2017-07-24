48eeec4feb47aeed2fb3678e9471fdb0abcff53b
hb5019 <sandye@hanmail.net>
2017-07-24 16:49:52 +0900

    최초 commit

---
+++ src/main/java/com/hanbit/there/api/ChaBackApiApplication.java
@@ -0,0 +1,12 @@
+package com.hanbit.there.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaBackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaBackApiApplication.class, args);
+	}
+}
